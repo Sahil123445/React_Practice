7bda307bae4279acde0d0cc3ae974ca5218f3bf8
Sahil Moosa <75231962+Sahil123445@users.noreply.github.com>
2023-08-28 01:14:34 -0400

    Add files via upload

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+//Should follow PascalCasing
+function Message() {
+  //JSX: JavaScript XML
+  const name = "";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello not Sahil</h1>;
+}
+
+export default Message;
